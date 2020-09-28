fd758c8b5d744d85b1439199acd99fea94d08ca7
Derek-microbit <71982968+Derek-microbit@users.noreply.github.com>
2020-09-28 08:31:13 -0400

    bluetooth connector

--- main.ts
+++ main.ts
@@ -1,3 +1,89 @@
-basic.forever(function () {
-	
+bluetooth.onBluetoothConnected(function () {
+    basic.showLeds(`
+        # # # # #
+        . # # # .
+        . . # . .
+        . # # # .
+        # # # # #
+        `)
+    basic.showLeds(`
+        . # # # #
+        . . # # #
+        . # # # .
+        # # # . .
+        # # # # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        # # . # #
+        # # # # #
+        # # . # #
+        # . . . #
+        `)
+    basic.showLeds(`
+        # # . . .
+        # # # . #
+        # # # # #
+        # . # # #
+        . . . # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        . # # # .
+        . . # . .
+        . # # # .
+        # # # # #
+        `)
+    basic.showLeds(`
+        . # # # #
+        . . # # #
+        . # # # .
+        # # # . .
+        # # # # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        # # . # #
+        # # # # #
+        # # . # #
+        # . . . #
+        `)
+    basic.showLeds(`
+        # # . . .
+        # # # . #
+        # # # # #
+        # . # # #
+        . . . # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        . # # # .
+        . . # . .
+        . # # # .
+        # # # # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        . . # . .
+        # # # # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . # .
+        # . # . .
+        . # . . .
+        `)
 })
+basic.showString("Pls connect Bluetooth ")
+basic.showString("Pls connect Bluetooth ")
+basic.showString("Pls connect Bluetooth ")
